e951e8215a2640f626959897e418bcc33b45d9a5
saaucorp <shuhaila@springrainit.com>
2019-07-18 20:34:15 +0600

    remove npm from sudo group

--- nodeEnv.sh
+++ nodeEnv.sh
@@ -44,13 +44,13 @@ docker --version &&
 printf  "\n=================================\n" &&
 sudo sudo ufw enable && 
 sudo sudo ufw default allow &&
-sudo chown -R $(whoami) ~/.npm &&
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 && 
 sudo echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list &&
 sudo apt-get update &&
 sudo apt-get install -y mongodb-org && 
 sudo systemctl start mongod &&
 sudo systemctl enable mongod &&
+printf  "\n\n\n=========== MONGO DB INSTALLED =============\n" &&
 sudo npm i -g pm2 &&
 
 printf  "\n\n\n=========== INSTALLATION COMPLETED =============\n"
