da63579fdc43ddf6dd9fdbc3a4ea38a0378dfd9a
saaucorp <shuhaila@springrainit.com>
2019-07-18 08:07:36 +0600

    Install mongo

--- nodeEnv.sh
+++ nodeEnv.sh
@@ -44,4 +44,9 @@ sudo npm i -g pm2 &&
 sudo sudo ufw enable && 
 sudo sudo ufw default allow &&
 sudo chown -R $(whoami) ~/.npm &&
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 && 
+sudo echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list &&
+sudo apt-get update &&
+sudo apt-get install -y mongodb-org && 
+sudo service mongod start &&
 printf  "\n\n\n=========== INSTALLATION COMPLETED =============\n"
