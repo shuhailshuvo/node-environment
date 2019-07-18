eb54bea2e3f77f1b80d6944de60ddaa80548655d
saaucorp <shuhaila@springrainit.com>
2019-07-18 07:59:52 +0600

    write jenkins source list

--- nodeEnv.sh
+++ nodeEnv.sh
@@ -23,7 +23,7 @@ printf  "\n\n\n=========== CARTBOT INSTALLED =============\n" &&
 certbot -h &&
 printf  "\n=================================\n" &&
 sudo wget --no-check-certificate -qO - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-printf  deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+echo  deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 sudo apt-get update && 
 sudo apt-get install -y jenkins &&  
 printf  "\n\n\n=========== JENKINS INSTALLED =============\n" &&
